fdd16bc22b1a9048cfb526478a71f20e777fad24
raafiAbdul <rafrafyunos@gmail.com>
2026-07-30 00:06:51 +0800

    added an updated view for the converter site (but only for length for now)

--- src/main/java/com/example/unit_converter/controllers/MainController.java
+++ src/main/java/com/example/unit_converter/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.example.unit_converter.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -8,33 +9,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class MainController {
 
     @GetMapping("/length")
-    public String getLength() {
-        return "length.html";
+    public String getLength(Model model) {
+        model.addAttribute("converterDisplay", "block");
+        model.addAttribute("answerDisplay", "none");
+        return "length";
     }
 
     @GetMapping("/weight")
     public String getWeight() {
-        return "weight.html";
+        return "weight";
     }
 
     @GetMapping("/temp")
     public String getTemp() {
-        return "temp.html";
+        return "temp";
     }
 
     @PostMapping("/length")
-    public String postLength() {
-        return "length.html";
+    public String postLength(Model model) {
+        model.addAttribute("converterDisplay", "none");
+        model.addAttribute("answerDisplay", "block");
+        return "length";
     }
 
     @PostMapping("/weight")
     public String postWeight() {
-        return "weight.html";
+        return "weight";
     }
 
     @PostMapping("/temp")
     public String postTemp() {
-        return "temp.html";
+        return "temp";
     }
 
 }
